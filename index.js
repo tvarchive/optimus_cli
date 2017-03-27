6ba2d0c46c8eadc72c209e1e0931f863e3f77ba9
krishnb <kbandame@gmail.com>
2017-03-27 19:59:06 +0530

    Added checkup command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const optimus = require('vorpal')();
 const http = require('http');
+const cmd=require('node-cmd');
 const hostname = '127.0.0.1';
 const port = 3000;
 var git = require("nodegit");
@@ -8,7 +9,8 @@ var mkdirp = require('mkdirp');
 var ProgressBar = require('ascii-progress');
 
 function createproject(args,callback) {
-  var projectfolder = __dirname+"/"+args.project_name;
+  var projectfolder = args.project_name;
+  console.log("creating project..");
   mkdirp(projectfolder, function (err) {
       if (err) console.error(err)
   });
@@ -29,12 +31,11 @@ var iv = setInterval(function () {
   if (bar.completed) {
     clearInterval(iv);
   }
-}, 250);
+}, 350);
 
  function progress() {
    iv;
  }
- callback();
 }
 
 function createtestfeed() {
@@ -48,6 +49,56 @@ function createtestfeed() {
     });
 }
 
+function setup(args,callback) {
+  cmd.get(
+    'java -version',
+    function(data,err) {
+    console.log('Verifying if java is installed');
+    if(!err) {
+      console.log('Found java');
+    } else {
+      console.log('Java is not installed, install it manually');
+    }
+  });
+
+  cmd.get(
+        'appium -v',
+        function(data,err){
+          console.log("Verifying if appium is installed");
+          if(!err) {
+            console.log('Found appium with version : ',data);
+          } else {
+            console.log("Appium is not installed, installing it now..");
+            cmd.get('npm install -g appium',function(data,err) {
+              if(!err) {
+                console.log('Installed appium successfully');
+              } else {
+                console.log('Failed to install appium, install it manually',err);
+              }
+            });
+          }
+        }
+    );
+
+    cmd.get(
+      'redis-cli -v',
+      function(data,err) {
+      console.log("Verifying if redis is installed");
+      if(!err) {
+        console.log('Found Redis with version:',data);
+      } else {
+        console.log("Redis is not installed, installing it now..");
+        cmd.get('brew install redis', function(data,err) {
+          if(!err) {
+            console.log('Installed redis successfully');
+          } else {
+            console.log('Failed to install redis, install it manually',err);
+          }
+        });
+      }
+    });
+}
+
 optimus
   .command('create project <project_name>', 'Create a new optimus project.')
   .action(createproject);
@@ -55,6 +106,10 @@ optimus
 optimus
   .command('create testfeed <testfeed_name>', 'Create a testfeed for the project')
   .action(createtestfeed);
+
+optimus
+  .command('checkup','sets up the optimus environment')
+  .action(setup);
 
 optimus
   .delimiter('optimus$')
